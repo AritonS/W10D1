901e569654b37a31c7e624498835e2c5bbfb2884
Lane Harris <lrharris215@gmail.com>
2021-03-01 12:15:47 -0500

    Adds add/remove class functions to DOMNodeCollection

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \************************************/
 /***/ ((module) => {
 
-eval("class DOMNodeCollection {\n    constructor(array) {\n        this.htmlArray = array;\n    }\n}\n\nmodule.exports = DOMNodeCollection\n\n//# sourceURL=webpack:///./src/dom_node_collection.js?");
+eval("class DOMNodeCollection {\n    constructor(array) {\n        this.htmlArray = array;\n    }\n\n    html(string) {\n        if(string !== undefined) {\n        this.htmlArray.forEach((node) => {\n            node.innerHTML = string;\n        })\n    }\n        else {\n            return this.htmlArray[0].innerHTML;\n        }\n    }\n\n    empty() {\n        this.htmlArray.forEach((node) => {\n            node.innerHTML = \"\";\n        });  \n    }\n    append(arg){\n        if (arg instanceof DOMNodeCollection) {\n            arg.htmlArray.forEach((arg) => {\n                this.htmlArray.forEach((node) => {\n                    node.innerHTML.concat(arg.outerHTML);\n                });\n            });\n        }else if (typeof arg === \"string\"){\n            this.htmlArray.forEach((node) => {\n                node.innerHTML += (arg);\n            });\n            \n        }else if (arg instanceof HTMLElement){\n            this.htmlArray.forEach((node) => {\n                node.innerHTML.concat(arg.outerHTML);\n            });\n        \n        }\n    }\n    addClass(newClass) {\n      \n        this.htmlArray.forEach((node) => {\n            node.className += ` ${newClass}`;\n        });\n        \n    }\n\n    removeClass(oldClass) {\n        this.htmlArray.forEach((node) => {\n            let classes = node.className.split(\" \");\n            if (classes.includes(oldClass)){\n                let oldIdx = classes.indexOf(oldClass);\n                classes.splice(oldIdx, 1);\n            }\n            node.className = classes.join(\" \");\n        });\n    }\n}\n\nmodule.exports = DOMNodeCollection\n\n//# sourceURL=webpack:///./src/dom_node_collection.js?");
 
 /***/ }),
 
@@ -25,7 +25,7 @@ eval("class DOMNodeCollection {\n    constructor(array) {\n        this.htmlArra
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("const DOMNodeCollection = __webpack_require__(/*! ./dom_node_collection.js */ \"./src/dom_node_collection.js\")\n\nwindow.$l = function(arg) {\n    if (typeof arg === 'string') {\n        let nodeList = document.querySelectorAll(arg)\n        return new DOMNodeCollection(Array.from(nodeList))\n    }\n}\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("const DOMNodeCollection = __webpack_require__(/*! ./dom_node_collection.js */ \"./src/dom_node_collection.js\")\n\nwindow.$l = function(arg) {\n    if (typeof arg === 'string') {\n        let nodeList = document.querySelectorAll(arg);\n        return new DOMNodeCollection(Array.from(nodeList));\n    }else if(arg instanceof HTMLElement) {\n        return new DOMNodeCollection(Array.from(arg))\n    }\n}\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -2,6 +2,60 @@ class DOMNodeCollection {
     constructor(array) {
         this.htmlArray = array;
     }
+
+    html(string) {
+        if(string !== undefined) {
+        this.htmlArray.forEach((node) => {
+            node.innerHTML = string;
+        })
+    }
+        else {
+            return this.htmlArray[0].innerHTML;
+        }
+    }
+
+    empty() {
+        this.htmlArray.forEach((node) => {
+            node.innerHTML = "";
+        });  
+    }
+    append(arg){
+        if (arg instanceof DOMNodeCollection) {
+            arg.htmlArray.forEach((arg) => {
+                this.htmlArray.forEach((node) => {
+                    node.innerHTML.concat(arg.outerHTML);
+                });
+            });
+        }else if (typeof arg === "string"){
+            this.htmlArray.forEach((node) => {
+                node.innerHTML += (arg);
+            });
+            
+        }else if (arg instanceof HTMLElement){
+            this.htmlArray.forEach((node) => {
+                node.innerHTML.concat(arg.outerHTML);
+            });
+        
+        }
+    }
+    addClass(newClass) {
+      
+        this.htmlArray.forEach((node) => {
+            node.className += ` ${newClass}`;
+        });
+        
+    }
+
+    removeClass(oldClass) {
+        this.htmlArray.forEach((node) => {
+            let classes = node.className.split(" ");
+            if (classes.includes(oldClass)){
+                let oldIdx = classes.indexOf(oldClass);
+                classes.splice(oldIdx, 1);
+            }
+            node.className = classes.join(" ");
+        });
+    }
 }
 
 module.exports = DOMNodeCollection
